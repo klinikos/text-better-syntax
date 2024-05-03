16a8f71f7ec37e4c3a2eb051515a937464610e19
Filipe Mosca <blog@mosca.me>
2024-05-03 12:26:44 -0300

    adding h2 detection and improving output redability

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# text-better-syntax
+# Text better syntax
+
+This is my custom markup language syntax. A combination between markdown and zim-wikitext. My objective is to create a simple markup language with suport to print colored text into html.
+
+This is my first program and I'm developing it as I learn Python.
+
--- app.py
+++ app.py
@@ -2,6 +2,17 @@
 
 import re
 
+template = """<!DOCTYPE html>
+<html>
+    <head>
+    <style>
+        pre{margin: 0 auto;}
+        h1{margin: 0 auto;}
+    </style>
+    <title>Arquivo TXT convertido para HTML</title>
+    </head>
+<body>"""
+
 def txt_to_html(input_file, output_file):
     # Abrir o arquivo de entrada para leitura
     with open(input_file, 'r') as f:
@@ -11,7 +22,7 @@ def txt_to_html(input_file, output_file):
     # Abrir o arquivo de saída para escrita
     with open(output_file, 'w') as f:
         # Escrever o cabeçalho do arquivo HTML
-        f.write("<!DOCTYPE html>\n<html>\n<head><style>pre{margin: 0 auto;}h1{margin: 0 auto;}</style>\n<title>Arquivo TXT convertido para HTML</title>\n</head>\n<body>\n")
+        f.write(template)
 
         # Variável para acompanhar se estamos dentro ou fora de uma tag <b>
         inside_bold = False
@@ -19,22 +30,26 @@ def txt_to_html(input_file, output_file):
         # Iterar sobre as linhas do arquivo de texto
         for line in text_content:
             # Verificar se a linha começa com #
-            if line.startswith("#"):
+            if line.startswith("##"):
                 # Se começar com #, escrever dentro de uma tag <h1>
-                f.write("<h1>")
+                f.write("\n\n<h2>")
                 f.write(line.lstrip("#").strip())  # Remover o # e espaços em branco
-                f.write("</h1>")
+                f.write("</h2>\n")
+            elif line.startswith("#"):
+                f.write("\n\n<h1>")
+                f.write(line.lstrip("#").strip())
+                f.write("</h1>\n")
             else:
                 # Caso contrário, substituir ** por <b> e </b>
                 line = re.sub(r'\*\*(.*?)\*\*', r'<b>\1</b>', line)
 
                 # Escrever a linha normalmente dentro de uma tag <pre>
                 f.write("<pre>")
-                f.write(line)
-                f.write("</pre>")
+                f.write(line.rstrip())
+                f.write("</pre>\n")
 
         # Escrever o final do arquivo HTML
-        f.write("</body>\n</html>")
+        f.write("\n</body>\n</html>")
 
 # Exemplo de uso
 input_file = 'exemplo.txt'  # Nome do arquivo de texto de entrada
